184efea63e131a4990b3ab439e83fc641b3d524e
8hatch8 <yutaka.pg@gmail.com>
2022-04-20 11:07:48 +0900

    add: router-viewを追加

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Main from "@/components/MainPage.vue";
+import Binding from "@/components/study/BindingPage.vue";
+import Directive from "@/components/study/DirectivePage.vue";
+import Event from "@/components/study/EventPage.vue";
 
-const routes = [];
+const routes = [
+  {
+    path: "/main",
+    name: "Main",
+    component: Main,
+  },
+  {
+    path: "/study/binding",
+    name: "binding",
+    component: Binding,
+  },
+  {
+    path: "/study/directive",
+    name: "directive",
+    component: Directive,
+  },
+  {
+    path: "/study/event",
+    name: "event",
+    component: Event,
+  },
+];
 
 // Vue-Routerの設定
 const router = createRouter({
