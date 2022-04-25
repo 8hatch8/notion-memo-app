174759878ee5d7e2343837506cda70f69d93f95d
8hatch8 <yutaka.pg@gmail.com>
2022-04-25 16:58:45 +0900

    add: ウィジェットコンポーネントを組み込み

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -29,11 +29,28 @@
         <div class="no-selected-note">ノートを選択してください</div>
       </template>
       <template v-else>
+        <!-- パス -->
         <div class="path">
           <small>{{ selectedPath }}</small>
         </div>
+        <!-- コンテンツ -->
         <div class="note-content">
           <h3 class="note-title">{{ selectedNote.name }}</h3>
+          <WidgetItem
+            v-for="widget in selectedNote.widgetList"
+            :key="widget.id"
+            :widget="widget"
+            :layer="1"
+            @input="onInputWidget"
+            @delete="onDeleteWidget"
+            @add-child="onAddChildWidget"
+            @add-brother="onAddBrotherWidget"
+          />
+          <!-- ウィジェット追加ボタン -->
+          <button class="transparent" @click="onClickButtonAddWidget">
+            <font-awesome-icon icon="plus-square" />
+            ウィジェット追加
+          </button>
         </div>
       </template>
     </div>
@@ -42,19 +59,13 @@
 
 <script>
 import NoteItem from "@/components/parts/NoteItem.vue";
+import WidgetItem from "@/components/parts/WidgetItem.vue";
 import draggable from "vuedraggable";
 export default {
-  components: { NoteItem, draggable },
+  components: { NoteItem, WidgetItem, draggable },
   data() {
     return {
-      noteList: [
-        {
-          id: 0,
-          name: "はじめに",
-          layer: 1,
-          children: [],
-        },
-      ],
+      noteList: [],
       selectedNote: null,
     };
   },
@@ -65,15 +76,14 @@ export default {
         for (let note of noteList) {
           // 引数pathがあればカレントパスに追加
           const currentPath = path == null ? note.name : `${path} / ${note.name}`;
-          // 目的のnoteが見つかればカレントパスを返す
+          // IF:目的のnoteが見つかればカレントパスを返す
           if (note.id === this.selectedNote.id) return currentPath;
-
-          // 見つからなければchildrenで再帰的に検索
+          // ELSE:見つからなければchildrenで再帰的に検索
           const selectedPath = searchSelectedPath(note.children, currentPath);
-          // 見つかれば結果を返す
+          // IF:見つかれば結果を返す
           if (selectedPath) return selectedPath;
         }
-        // 再起的に検索しても見つからない場合
+        // 再起的に検索しても見つからない場合、falseを返す
         return false;
       };
       // 実行
@@ -81,9 +91,11 @@ export default {
     },
   },
   methods: {
+    // 【NOTE】
     onSelectNote(note) {
       this.selectedNote = note;
     },
+    // ノート追加の共通メソッド
     addNote(targetList, layer, index) {
       layer = layer || 1;
       const note = {
@@ -91,7 +103,11 @@ export default {
         name: `新規ノート-${layer}-${targetList.length + 1}`,
         children: [],
         layer: layer,
+        widgetList: [],
       };
+      // ウィジェットリストを追加
+      this.addWidget(note.widgetList);
+      // 挿入位置の条件分岐
       if (index == null) {
         targetList.push(note);
       } else {
@@ -129,6 +145,46 @@ export default {
       if (this.selectedNote === note) {
         this.onSelectNote(editedNote);
       }
+    },
+    // 【WIDGET】
+    onInputWidget(text, widget) {
+      widget.text = text;
+    },
+    addWidget(targetList, layer, index) {
+      layer = layer || 1;
+      const widget = {
+        id: new Date().getTime().toString(16),
+        type: "heading",
+        text: "",
+        children: [],
+        layer: layer,
+      };
+      // 挿入位置の条件分岐
+      if (index == null) {
+        targetList.push(widget);
+      } else {
+        targetList.splice(index + 1, 0, widget);
+      }
+    },
+    onClickButtonAddWidget() {
+      this.addWidget(this.selectedNote.widgetList);
+    },
+    onAddChildWidget(widget) {
+      this.addWidget(widget.children, widget.layer + 1);
+    },
+    onAddBrotherWidget(parentWidget, widget) {
+      // parentWidget || this.selectedNote.widgetでいいのでは
+      const targetList =
+        parentWidget == null ? this.selectedNote.widgetList : parentWidget.children;
+      const layer = parentWidget == null ? null : parentWidget.layer + 1;
+      const index = targetList.indexOf(widget);
+      this.addWidget(targetList, layer, index);
+    },
+    onDeleteWidget(parentWidget, widget) {
+      const targetList =
+        parentWidget == null ? this.selectedNote.widgetList : parentWidget.children;
+      const index = targetList.indexOf(widget);
+      targetList.splice(index, 1);
     },
   },
 };
